a013b3b02dbd0e256ed9c881d6bfe868be1d0a65
BakkappaN <bakkappa123@yahoo.com>
2019-02-14 10:58:13 +0530

    Added java file.

---
+++ JavaInterviewQuestionsAnswers/src/com/programs/InterviewJavaPrograms/SampleJavaProgram2.java
@@ -0,0 +1,14 @@
+package com.programs.InterviewJavaPrograms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
+public class SampleJavaProgram2 
+{
+	public static void main(String[] args) 
+	{}	
+
+}
